175a1c37a5fa5619d1e4199ad4bf0fd97a4123d0
metalfatigue <htavenido@gmail.com>
2014-09-05 08:38:36 +0800

    I still do not understand why it is halved

--- calculus/integral.js
+++ calculus/integral.js
@@ -1,9 +1,9 @@
-(function integral_circle () {
-  for (var ct = 0; ct <= 0.5; ct += 0.1) {
-    console.log((ring_strategy(ct) * ct) / 2);
+(function integral(strategyfn, start, end, increment) {
+  for (var ct = start; ct <= end; ct += increment) {
+    console.log(strategyfn(ct));
   }
-})();
+})(ring_strategy, 0, 0.5, 0.1);
 
 function ring_strategy(r) {
-  return 2 * Math.PI * r;
+  return ((2 * Math.PI * r) * r) / 2;
 }
