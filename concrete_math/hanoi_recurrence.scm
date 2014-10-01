af77466b986ebc9472b874b9d2b890927531da9d
metalfatigue <htavenido@gmail.com>
2014-10-02 02:14:26 +0800

    refactor into small, focused pieces

--- concrete_math/hanoi_recurrence.scm
+++ concrete_math/hanoi_recurrence.scm
@@ -3,5 +3,14 @@
       1
       (+ 1 (* 2 (hanoi (- x 1))))))
 
+(define (hanoi-i x)
+  (define (iter y z)
+    (if (= z x)
+        y 
+        (iter (+ 1 (* 2 z)) (+ 1 z))))
+  (iter 0 0))
+     
 (= 3 (hanoi 2))
 (= 7 (hanoi 3))
+
+(hanoi-i 3)
